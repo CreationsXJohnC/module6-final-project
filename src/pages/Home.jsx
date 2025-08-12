eb803832aa3f8c50278e334e9347bb408ffd9926
Che <johnccreations21@gmail.com>
2025-08-11 20:51:04 -0400

    fourth update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
     useEffect(() => {
@@ -6,7 +7,42 @@ const Home = () => {
     }, []);
 
     return (
-        <div>Home</div>
+        <><div>Home</div>
+            <nav>
+                <div className="nav__bar">
+                    <img className="movie__logo" src="./assets/black white clean movie logo-removebg.png" alt="" />
+                    <ul className="nav__links">
+                        <li>
+                            <Link href="#" className="nav__link">Home</Link>
+                        </li>
+                        <li>
+                            <Link href="#" className="nav__link">Browse</Link>
+                        </li>
+                        <li>
+                            <Link href="#" className="nav__link">Details</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <footer>
+                <div className="footer__container">
+                    <div className="row__column">
+                        <a href="#">
+                            <figure className="footer__logo">
+                                <img className="footer__logo--img" src="./assets/black white clean movie logo.png" alt=""> </img>
+                            </figure>
+                        </a>
+                        <div className="footer__list">
+                            <Link href="" className="footer__link">Home</Link>
+                            <Link href="" className="footer__link">Browse</Link>
+                            <Link href="" className="footer__link">Details</Link>
+                        </div>
+                        <div className="footer__copyright">Copyright © 2025 Movie Entertainment</div>
+                    </div>
+                </div>
+            </footer></>
+
     );
 };
 
